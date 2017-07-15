657a83c4df0fbc3880e205e18e32b3b94e610afe
Zhihao Jian <jianzhihao123@gmail.com>
2017-07-15 22:10:01 +0800

    第三次修改，首页改版以及修正若干bug

---
+++ dist/js/my-app.js
@@ -0,0 +1,24 @@
+var app = new Framework7({
+    pushState: true,
+    swipePanel: 'left',
+    // hideNavbarOnPageScroll: true
+});
+
+var $$ = Dom7;
+
+//下拉弹窗
+$$('.open-plus').on('click', function () {
+    var clickedPlus = this;
+    app.popover('.popover-links', clickedPlus);
+})
+
+//轮播图
+var mySwiper3 = app.swiper('.swiper-3', {
+    pagination: '.swiper-3 .swiper-pagination',
+    spaceBetween: 10,
+    slidesPerView: 3
+});
+
+$$('.open-login-screen').on('click', function () {
+    app.loginScreen();
+})
