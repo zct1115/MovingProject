04d7fa1ee45025160d85a635c44343d9f935f616
Zhihao Jian <jianzhihao123@gmail.com>
2017-07-15 08:17:59 +0800

    第二次修改，将index中的标签页拆分到子页面，使用ajax加载

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -2,19 +2,37 @@ var app = new Framework7({
     pushState: true,
     swipePanel: 'left',
 });
+
 var $$ = Dom7;
 
-// $$('.open-left-panel').on('click', function (e) {
-//     app.openPanel('left');
-// })
-
+//下拉弹窗
 $$('.open-plus').on('click', function () {
     var clickedPlus = this;
     app.popover('.popover-links', clickedPlus);
 })
 
+//轮播图
 var mySwiper3 = app.swiper('.swiper-3', {
     pagination: '.swiper-3 .swiper-pagination',
     spaceBetween: 10,
     slidesPerView: 3
 });
+
+//发现标签页ajax加载
+$$('#tab2').on('tab:show', function () {
+    app.addView('#tab2').router.loadPage('find.html');
+})
+
+//通讯录标签页ajax加载
+$$('#tab3').on('tab:show', function () {
+    app.addView('#tab3').router.loadPage('communicate.html');
+})
+
+$$('#tab4').on('tab:show', function () {
+    app.addView('#tab4').router.loadPage('myInfo.html');
+})
+
+$$('.open-login-screen').on('click', function () {
+    // app.addView('.view-main').router.load('login.html');
+    app.loginScreen();
+})
