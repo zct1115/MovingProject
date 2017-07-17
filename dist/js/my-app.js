796013013d97438cb8a409efdbd696b75961a909
Zhihao Jian <jianzhihao123@gmail.com>
2017-07-17 09:46:58 +0800

    '修复若干bug'

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -12,6 +12,12 @@ $$('.open-plus').on('click', function () {
     app.popover('.popover-links', clickedPlus);
 })
 
+//启动页面轮播
+var mySwiper = app.swiper('.swiper-start', {
+    speed: 400,
+    pagination: '.swiper-start .swiper-pagination'
+});
+
 //轮播图
 var mySwiper3 = app.swiper('.swiper-3', {
     pagination: '.swiper-3 .swiper-pagination',
@@ -21,4 +27,8 @@ var mySwiper3 = app.swiper('.swiper-3', {
 
 $$('.open-login-screen').on('click', function () {
     app.loginScreen();
+})
+
+$$('.user_detail').on('click', function () {
+    app.addView('.view').router.loadPage('../user_detail.html')
 })
